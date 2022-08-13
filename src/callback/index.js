45935d78fc7fb84aa17499bded03eddc3d1218c6
ackermanGitHub <julio.sergio2709@gmail.com>
2022-08-13 15:07:51 -0400

    XMLHttpRequest

--- src/callback/index.js
+++ src/callback/index.js
@@ -14,3 +14,14 @@ function calc(a, b, callback) {
     return callback(a, b);
 }
 console.log(calc(2, 2, mult));
+console.log(calc(3, 4, (a, b) => {return `El primer número es ${a}, el segundo ${b}`}));
+
+setTimeout(() => {
+    console.log("Hello JavaScript");
+}, 5000)
+
+function gretting(name) {
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(gretting, 2000, 'Julio');
---
+++ src/challenge.js/challenge.js
@@ -0,0 +1,19 @@
+const XMLHttpRequest = require('xmlhttprequest');
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi, callback) {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', urlAPI, true);
+    xhttp.onreadystatechange = (event) => {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText));
+            }
+        } else {
+            const error = new Error('Error' + urlApi);
+            return callback(error, null);
+        }
+    }
+    xhttp.send();
+}
